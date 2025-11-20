d4ba6c44bc211822b1ed62e020a7965f9739c26d
bernddei <115920576+bernddei@users.noreply.github.com>
2025-11-20 09:01:11 +0100

    Update run.sh

--- oiluhr/run.sh
+++ oiluhr/run.sh
@@ -1,3 +1,15 @@
-#!/usr/bin/env bash
-echo "OilUhr Add-on gestartet"
+#!/usr/bin/with-contenv bashio
+set -e
+
+bashio::log.info "OilUhr OCR Add-on wird gestartet..."
+
+# Lade Konfiguration
+ESP_IP=$(bashio::config 'esp_ip')
+POLL_INTERVAL=$(bashio::config 'poll_interval')
+DEBUG=$(bashio::config 'debug')
+
+bashio::log.info "ESP32-CAM IP: ${ESP_IP}"
+bashio::log.info "Poll-Intervall: ${POLL_INTERVAL} Sekunden"
+
+# Starte Python-Skript
 python3 /app/main.py
